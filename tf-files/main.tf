6ceba930409637f4062e264e76b2727b34a642e9
ilker <ilkeradak35@gmail.com>
2022-08-15 02:28:25 +0300

    user-data.sh and main.tf is revised

--- tf-files/main.tf
+++ tf-files/main.tf
@@ -106,7 +106,7 @@ resource "aws_db_instance" "db-server" {
 resource "github_repository_file" "dbendpoint" {
   content = aws_db_instance.db-server.address
   file = "dbserver.endpoint"
-  repository = "phonebook"
+  repository = "202-Terraform-Phonebook-Application-deployed-on-AWS"
   overwrite_on_create = true
   branch = "main"
 }
--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -5,5 +5,12 @@ pip3 install flask
 pip3 install flask_mysql
 yum install git -y
 TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/Ilkeradak/phonebook.git
+cd /home/ec2-user 
+mkdir phonebook && cd phonebook
+wget https://raw.githubusercontent.com/Ilkeradak/202-Terraform-Phonebook-Application-deployed-on-AWS/main/dbserver.endpoint
+wget https://raw.githubusercontent.com/Ilkeradak/202-Terraform-Phonebook-Application-deployed-on-AWS/main/phonebook/phonebook-app.py
+mkdir templates && cd templates
+wget https://raw.githubusercontent.com/Ilkeradak/202-Terraform-Phonebook-Application-deployed-on-AWS/main/phonebook/templates/add-update.html
+wget https://raw.githubusercontent.com/Ilkeradak/202-Terraform-Phonebook-Application-deployed-on-AWS/main/phonebook/templates/delete.html
+wget https://raw.githubusercontent.com/Ilkeradak/202-Terraform-Phonebook-Application-deployed-on-AWS/main/phonebook/templates/index.html
 python3 /home/ec2-user/phonebook/phonebook-app.py
